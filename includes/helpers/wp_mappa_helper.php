7a1ef109f7b56fd2de5d96b075d4d631f15b7b45
Rado <radovan.smitala@gmail.com>
2020-08-02 09:54:54 +0200

    Update wp_mappa_helper.php

--- includes/helpers/wp_mappa_helper.php
+++ includes/helpers/wp_mappa_helper.php
@@ -34,66 +34,66 @@ class WpMappaHelper
         return $repo->getById($mappaId);
     }
 
-    public static function updatePlace(array $mappaObject, array $options) : \WP_Post
+    public static function updatePlace(array $mappaObject, array $options) : GeoPlaceManager
     {
         return ManagerFactory::processSingleData($mappaObject, MAPPA_GEO_PLACE, ['language' => $options['language']]);
     }
 
-    public static function updateRoute(array $mappaObject, array $options) : \WP_Post
+    public static function updateRoute(array $mappaObject, array $options) : GeoRouteManager
     {
         return ManagerFactory::processSingleData($mappaObject, MAPPA_GEO_ROUTE, ['language' => $options['language']]);
     }
 
-    public static function updateCategory(array $mappaObject, array $options)
+    public static function updateCategory(array $mappaObject, array $options) : GeoCategoryManager
     {
         return ManagerFactory::processSingleData($mappaObject, MAPPA_GEO_CATEGORY, [
         'language' => $options['language'],
     ]);
     }
 
-    public static function updateCategoryGroup(array $mappaObject, array $options)
+    public static function updateCategoryGroup(array $mappaObject, array $options) : GeoCategoryGroupManager
     {
         return ManagerFactory::processSingleData($mappaObject, MAPPA_GEO_CATEGORY_GROUP, [
         'language' => $options['language'],
     ]);
     }
 
-    public static function updateEvent(array $mappaObject, array $options)
+    public static function updateEvent(array $mappaObject, array $options) : MessageEventManager
     {
         return ManagerFactory::processSingleData($mappaObject, MAPPA_MESSAGE_EVENT, [
         'language' => $options['language'],
     ]);
     }
 
-    public static function fetchAndUpdatePlace(int $postId, array $options) : \WP_Post
+    public static function fetchAndUpdatePlace(int $postId, array $options) : GeoPlaceManager
     {
         $mappaId     = \get_post_meta($postId, '_mappa_id', true);
         $mappaObject = self::getPlaceById($mappaId, ['language' => $options['language']]);
         return self::updatePlace($mappaObject, ['language' => $options['language']]);
     }
 
-    public static function fetchAndUpdateRoute(int $postId, array $options) : \WP_Post
+    public static function fetchAndUpdateRoute(int $postId, array $options) : GeoRouteManager
     {
         $mappaId     = \get_post_meta($postId, '_mappa_id', true);
         $mappaObject = self::getRouteById($mappaId, ['language' => $options['language']]);
         return self::updateRoute($mappaObject, ['language' => $options['language']]);
     }
 
-    public static function fetchAndUpdateEvent(int $postId, array $options) : \WP_Post
+    public static function fetchAndUpdateEvent(int $postId, array $options) : MessageEventManager
     {
         $mappaId     = \get_post_meta($postId, '_mappa_id', true);
         $mappaObject = self::getEventById($mappaId, ['language' => $options['language']]);
         return self::updateEvent($mappaObject, ['language' => $options['language']]);
     }
 
-    public static function fetchAndUpdateCategory(int $termId, array $options) : \WP_Term
+    public static function fetchAndUpdateCategory(int $termId, array $options) : GeoCategoryManager
     {
         $mappaId     = \get_post_meta($termId, '_mappa_id', true);
         $mappaObject = self::getCategoryById($mappaId, ['language' => $options['language']]);
         return self::updateCategory($mappaObject, ['language' => $options['language']]);
     }
 
-    public static function fetchAndUpdateCategoryGroup(int $termId, array $options) : \WP_Term
+    public static function fetchAndUpdateCategoryGroup(int $termId, array $options) : GeoCategoryGroupManager
     {
         $mappaId     = \get_post_meta($termId, '_mappa_id', true);
         $mappaObject = self::getCategoryGroupById($mappaId, ['language' => $options['language']]);
